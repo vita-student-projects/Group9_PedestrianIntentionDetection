3e95a6c81c74ba4887eca6e7abed0fbff1187a7e
Arina Rak <arina.ruck@gmail.com>
2023-05-31 12:53:30 +0200

    cnn only eval

--- eval_hybrid.py
+++ eval_hybrid.py
@@ -1,11 +1,9 @@
 import argparse
-import numpy as np
 import torch
 from tqdm import tqdm
-from sklearn.metrics import average_precision_score, classification_report, f1_score, precision_score, recall_score
-from src.dataset.intention.jaad_dataset import build_pedb_dataset_jaad, subsample_and_balance, unpack_batch
+from src.dataset.intention.jaad_dataset import build_pedb_dataset_jaad_new, unpack_batch
 from src.early_stopping import load_from_checkpoint
-from src.model.models import Res18Classifier
+from src.model.models import Res18Classifier, RNNClassifier
 from src.dataset.loader import define_path, IntentionSequenceDataset
 from src.transform.preprocess import ImageTransform, Compose, CropBoxWithBackgroud
 from src.utils import prep_pred_storage, print_eval_metrics
@@ -13,6 +11,9 @@ import torchvision.transforms as transforms
 
 MEAN = [0.3104, 0.2813, 0.2973]
 STD = [0.1761, 0.1722, 0.1673]
+
+EMBEDDING_DIM = 256
+EVAL_MODES = ['cnn_only']
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -25,7 +26,7 @@ def get_args():
                         metavar='FPS', help='sampling rate(fps)')
     parser.add_argument('--max-frames', default=5, type=int,
                         help='maximum number of frames in histroy sequence')
-    parser.add_argument('--pred', default=10, type=int,
+    parser.add_argument('--pred', default=5, type=int,
                         help='prediction length, predicting-ahead time')
     parser.add_argument('--balancing-ratio', default=1.0, type=float,
                         help='ratio of balanced instances(1/0)')
@@ -33,20 +34,16 @@ def get_args():
                         help='jitter bbox for cropping')
     parser.add_argument('-s', '--seed', type=int, default=99,
                         help='set random seed for sampling')
-    parser.add_argument('--checkpoint-path', type=str,
+    parser.add_argument('-cp', '--checkpoint-path', type=str,
                         help='path to the checkpoint for loading pretrained weights')
+    parser.add_argument("--mode", type=str)
     args = parser.parse_args()
 
     return args
 
 
 def build_loader(args, intent_seqs, TRANSFORM, image_dir):
-    dataset = subsample_and_balance(
-        intent_seqs,
-        balance=False, 
-        max_frames=args.max_frames, 
-        seed=args.seed)
-    ds = IntentionSequenceDataset(dataset, image_dir=image_dir, hflip_p = 0, preprocess=TRANSFORM)
+    ds = IntentionSequenceDataset(intent_seqs, image_dir=image_dir, hflip_p = 0, preprocess=TRANSFORM)
     loader = torch.utils.data.DataLoader(ds, batch_size=1, shuffle=False)
     return loader
 
@@ -64,41 +61,62 @@ def eval_cnn(loader, model, device):
         preds[step] = outputs_CNN.detach().cpu().squeeze()
         tgts[step] = targets.detach().cpu().squeeze()
 
-    print_eval_metrics(preds, tgts, encoder_CNN.best_threshold)
+    print_eval_metrics(tgts, preds, model['best_thr'])
+
+
+@torch.no_grad()
+def eval_rnn(loader, model, device):
+    # swith to evaluate mode
+    decoder_RNN = model['decoder']
+    preds, tgts, _, _ = prep_pred_storage(loader)
+
+    for step, inputs in enumerate(tqdm(loader)):
+        _, seq_len, pos_vel, _, _, targets = unpack_batch(inputs, device)
+        outputs_CNN = decoder_RNN(pos_vel, seq_len).squeeze(-1)
+        
+        preds[step] = outputs_CNN.detach().cpu().squeeze()
+        tgts[step] = targets.detach().cpu().squeeze()
+
+    print_eval_metrics(tgts, preds, model['best_thr'])
 
 
 def main():
     args = get_args()
+    if args.mode not in EVAL_MODES:
+        raise ValueError(f'invalid mode, please choose from {", ".join(EVAL_MODES)}')
     # loading data
     anns_paths_eval, image_dir_eval = define_path(use_jaad=args.jaad, use_pie=False, use_titan=False)
 
-    normal_intent_sequences = build_pedb_dataset_jaad(
+    normal_intent_sequences = build_pedb_dataset_jaad_new(
         anns_paths_eval["JAAD"]["anns"], 
         anns_paths_eval["JAAD"]["split"], 
         image_set = "test", 
         fps=args.fps,
         prediction_frames=args.pred, 
+        max_frames=args.max_frames,
         verbose=True)
 
-    hard_intent_sequences = build_pedb_dataset_jaad(
+    hard_intent_sequences = build_pedb_dataset_jaad_new(
         anns_paths_eval["JAAD"]["anns"], 
         anns_paths_eval["JAAD"]["split"], 
         image_set = "test", 
         fps=args.fps,
         prediction_frames=args.pred, 
+        max_frames=args.max_frames,
         verbose=True,
         transition_only=True)
+    
 
     # load model
 
-    if args.mode == 'CNN_only':
+    if args.mode == 'cnn_only':
 
-        encoder_res18 = Res18Classifier(CNN_embed_dim=256, activation="sigmoid").to(device)
+        encoder_res18 = Res18Classifier(CNN_embed_dim=EMBEDDING_DIM, activation="sigmoid").to(device)
         encoder_res18.eval()
         model = {'encoder': encoder_res18}
-        load_from_checkpoint(model, args.checkpoint_path)
 
         crop_with_background = CropBoxWithBackgroud(size=224)
+
         TRANSFORM = Compose([
             crop_with_background,
             ImageTransform(
@@ -108,6 +126,16 @@ def main():
                 ]),
             ) 
         ])
+    
+    elif args.mode == 'rnn_only':
+
+        POS_VEL_DIM = 4
+        rnn_classifier = RNNClassifier(input_size=POS_VEL_DIM, rnn_embeding_size=EMBEDDING_DIM, classification_head_size=128).to(device)
+        model = {'decoder': rnn_classifier}
+        TRANSFORM = None
+
+    load_from_checkpoint(model, args.checkpoint_path)    
+
 
     normal_loader = build_loader(args, normal_intent_sequences, TRANSFORM, image_dir_eval)
     hard_loader = build_loader(args, hard_intent_sequences, TRANSFORM, image_dir_eval)
